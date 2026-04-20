dd99e069e429315feb5396ae450e420f6d269387
Akaalroop Singh <akaal@akaalroop.com>
2026-04-20 19:05:48 +0100

    Moved all of the word_to_number stuff to a separate file

---
+++ src/word_to_number.rs
@@ -0,0 +1,96 @@
+use std::io;
+
+pub (crate) fn change_word_to_number() {
+    let mut word_number = String::new();
+
+    println!("Please enter the number in words which you wish to convert:");
+    io::stdin().read_line(&mut word_number)
+        .expect("Error reading input");
+
+    word_number = String::from(word_number.strip_suffix("\n").unwrap());
+
+    let number: u16;
+
+    if word_number.contains("and") {
+        let numbers = word_number.split("and");
+        let mut temp_number = 1;
+        for mut number in numbers {
+            if number.starts_with(' ') {
+                number = number.strip_prefix(' ').expect("If this gets called something's gone really wrong");
+            }
+            if number.contains(' ') {
+                let numbers = number.split_ascii_whitespace();
+                for number in numbers {
+                    if !number.eq(" ") {
+                        temp_number *= exchange_word_for_number(number);
+                    }
+                }
+            }
+            if number.contains('-') {
+                let numbers = number.split("-");
+                for number in numbers {
+                    temp_number += exchange_word_for_number(number);
+                }
+            }
+        }
+        number = temp_number;
+    }
+    else if word_number.contains('-') {
+        let numbers = word_number.split('-');
+        let mut temp_number = 0;
+        for number in numbers {
+            temp_number += exchange_word_for_number(number);
+        }
+        number = temp_number;
+    }
+    else if word_number.contains("hundred") {
+        let mut temp_number = 1;
+        if word_number.contains(' ') {
+            let numbers = word_number.split_ascii_whitespace();
+            for number in numbers {
+                if !number.eq(" ") {
+                    temp_number *= exchange_word_for_number(number);
+                }
+            }
+        }
+        number = temp_number
+    }
+    else {
+        number = exchange_word_for_number(&*word_number);
+    }
+
+    println!("You typed in {word_number} which is actually {number}")
+}
+fn exchange_word_for_number(number_word: &str) -> u16 {
+    match number_word {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        "ten" => 10,
+        "eleven" => 11,
+        "twelve" => 12,
+        "thirteen" => 13,
+        "fourteen" => 14,
+        "fifteen" => 15,
+        "sixteen" => 16,
+        "seventeen" => 17,
+        "eighteen" => 18,
+        "nineteen" => 19,
+        "twenty" => 20,
+        "thirty" => 30,
+        "forty" => 40,
+        "fifty" => 50,
+        "sixty" => 60,
+        "seventy" => 70,
+        "eighty" => 80,
+        "ninety" => 90,
+        "hundred" => 100,
+        _ => panic!("NUMBER ({number_word}) NOT VALID")
+    }
+}
