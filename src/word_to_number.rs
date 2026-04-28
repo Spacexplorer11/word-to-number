b63e669651f25d08cf2c343bacd547812da4f4e7
Akaalroop Singh <akaal@akaalroop.com>
2026-04-28 21:55:41 +0100

    Semi-working approach. We support big numbers now! But not multiplied big numbers like hundred thousand

--- src/word_to_number.rs
+++ src/word_to_number.rs
@@ -6,75 +6,74 @@ pub(crate) enum WordToNumberError {
 use crate::word_to_number::WordToNumberError::{BadRequest, InternalServer};
 
 pub(crate) fn change_word_to_number(word_number: &str) -> Result<u64, WordToNumberError> {
-    let number: u64;
+    let mut number: u64 = 0;
     let word_number = word_number.to_lowercase();
-
+    let mut word_numbers_mapped: Vec<Option<u64>> = Vec::new();
+    let mut multiplier_indexes: Vec<usize> = Vec::new();
     if word_number.contains(" and ") {
-        let numbers = word_number.split("and");
-        let mut temp_number = 1;
-        for mut number in numbers {
-            if number.starts_with(' ') {
-                number = match number.strip_prefix(' ') {
-                    Some(number) => number,
-                    None => return Err(InternalServer),
-                }
+        let parts = word_number.split(" and ").collect::<Vec<&str>>();
+        word_numbers_mapped = parts
+            .get(0)
+            .unwrap()
+            .split_whitespace()
+            .map(|number| exchange_word_for_number(number))
+            .collect::<Vec<Option<u64>>>();
+        let numbers_after_and = match parts.get(1) {
+            Some(smth) => smth,
+            None => "",
+        };
+        if numbers_after_and.contains('-') {
+            let numbers = numbers_after_and.split('-').collect::<Vec<&str>>();
+            for number in numbers {
+                word_numbers_mapped.push(exchange_word_for_number(number));
             }
-            if number.contains(' ') {
-                let numbers = number.split_ascii_whitespace();
-                for number in numbers {
-                    if !number.eq(" ") {
-                        temp_number *= match exchange_word_for_number(number) {
-                            Some(num) => num,
-                            None => return Err(BadRequest),
-                        };
-                    }
-                }
-            }
-            if number.contains('-') {
-                let numbers = number.split("-");
-                for number in numbers {
-                    temp_number += match exchange_word_for_number(number) {
-                        Some(num) => num,
-                        None => return Err(BadRequest),
-                    };
-                }
-            } else if exchange_word_for_number(number).is_some() {
-                temp_number += match exchange_word_for_number(number) {
-                    Some(num) => num,
-                    None => return Err(BadRequest), // This can't be triggered anyway because 3 lines up we check
-                }
-            }
+        } else {
+            word_numbers_mapped.push(exchange_word_for_number(numbers_after_and))
         }
-        number = temp_number;
     } else if word_number.contains('-') {
-        let numbers = word_number.split('-');
-        let mut temp_number = 0;
+        let numbers = word_number.split('-').collect::<Vec<&str>>();
         for number in numbers {
-            temp_number += match exchange_word_for_number(number) {
+            word_numbers_mapped.push(exchange_word_for_number(number));
+        }
+    } else {
+        word_numbers_mapped = word_number
+            .split_whitespace()
+            .map(|number| exchange_word_for_number(number))
+            .collect::<Vec<Option<u64>>>();
+    }
+    for (index, number) in word_numbers_mapped.iter().enumerate() {
+        let number = match number {
+            Some(number) => *number,
+            None => return Err(BadRequest),
+        };
+        if is_multiplier_number(number) {
+            multiplier_indexes.push(index)
+        }
+    }
+
+    let mut elements_removed: usize = 0;
+    let mut numbers_to_add = Vec::new();
+    for mut index in multiplier_indexes {
+        index -= elements_removed;
+        let mut temp_number = 1;
+        for i in 0..=index {
+            temp_number *= word_numbers_mapped[i].unwrap(); // This is safe cuz the whole vector is checked above
+        }
+        numbers_to_add.push(temp_number);
+        elements_removed += index + 1; // Added one since the elements removed != index since index starts at 0.
+        word_numbers_mapped.drain(0..=index);
+    }
+    if !word_numbers_mapped.is_empty() {
+        for num in word_numbers_mapped {
+            let num = match num {
                 Some(num) => num,
                 None => return Err(BadRequest),
             };
+            number += num
         }
-        number = temp_number;
-    } else if word_number.contains("hundred") {
-        let mut temp_number = 1;
-        if word_number.contains(' ') {
-            let numbers = word_number.split_ascii_whitespace();
-            for number in numbers {
-                if !number.eq(" ") {
-                    temp_number *= match exchange_word_for_number(number) {
-                        Some(num) => num,
-                        None => return Err(BadRequest),
-                    };
-                }
-            }
-        }
-        number = temp_number
-    } else {
-        number = match exchange_word_for_number(&word_number) {
-            Some(num) => num,
-            None => return Err(BadRequest),
-        };
+    }
+    for num in numbers_to_add {
+        number += num
     }
     Ok(number)
 }
@@ -108,6 +107,11 @@ fn exchange_word_for_number(number_word: &str) -> Option<u64> {
         "eighty" => Some(80),
         "ninety" => Some(90),
         "hundred" => Some(100),
+        "thousand" => Some(1000),
         _ => None,
     }
+}
+
+fn is_multiplier_number(number: u64) -> bool {
+    number.to_string().ends_with("00")
 }
